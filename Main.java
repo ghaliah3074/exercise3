3c9ad805de55526159c3986f5a325d79b3571d44
ghaliah3074 <148686086+ghaliah3074@users.noreply.github.com>
2023-10-26 08:06:43 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,142 @@
+import java.util.Scanner;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        Scanner a = new Scanner(System.in);
+
+//        for(int num0=1; num0<=100;num0++) {
+//
+//            System.out.println(" pls enter number ");
+//            num0=a.nextInt();
+//
+//
+//            if (num0% 3 == 0 && num0%5==0) {
+//                System.out.println("FizzBuzz");
+//                break;
+//            } else if (num0 % 5 == 0) {
+//                System.out.println("Buzz");
+//                break;
+//            } else if (num0% 3 == 0) {
+//                System.out.println("Fizz");
+//                break;
+//            } else {
+//                System.out.println(num0);
+//                continue;
+//            }
+//        }
+
+
+        //q2
+        ;
+
+//        char[] dd=ll.toCharArray();
+//        String ll="the quick brown for Expected";
+//
+//        for (int i =ll.length()-1 ; i>=0; i--) {
+//
+//            ll=ll+a.
+//            System.out.println(" reverse string "+ dd [i]);
+//        break;
+//        }
+
+
+//    //q3
+//        int num;
+//        int fact=1;
+//        System.out.println("pls enter a number");
+//        num=a.nextInt();
+//        for(int i=1; i<=num;i++){
+//        {
+//            fact *=i;
+//
+//        }
+//        System.out.println("factorial:   "+  fact );
+//
+
+
+        //q4
+//        System.out.println("pls enter the base number:   ");
+//        int base = a.nextInt();
+//        System.out.println("pls enter the power number:  ");
+//        int power = a.nextInt();
+//
+//        int result = 1;
+//        for (int i = 1; i <= power; i++) {
+//            {
+//                result *= base;
+//            }
+//            System.out.println("result:   " + result);
+
+
+        //q5
+
+//        int numa;
+//
+//        int sumeven=0;
+//        int sumodd=0;
+//            System.out.println("enter integer ");
+//            numa=a.nextInt(i);
+//
+//
+//        for ( numa=1; i != 0 ; i=i){
+//            if( i%2 ==0)
+//                sumeven =sumeven+i;
+//            else
+//                sumodd= sumodd +i;
+//        }
+//
+//
+//            System.out.println("the even sum is :   "+sumeven);
+//            System.out.println(" the odd sum is  :   "+sumodd);
+//
+//        }
+
+
+        //q6
+//
+//        int n;
+//        boolean isprime;
+//        do {
+//            System.out.println(" enter positive integer :   ");
+//            n = a.nextInt();
+//        }
+//        while (n <= 1);
+//        System.out.println(" the number is prime:    " + n);
+//        for (int i = 0; i <= n; i++){
+//            isprime = true;
+//
+//
+//        for (int j=0;j<= i /2; j++);{
+//
+//                if(i = i % j== 0) {
+//                isprime = false;
+//                break;
+//            }
+//            }
+//
+//                if (isprime){
+//                System.out.println(i +" ");
+//
+//            }
+//            }
+
+
+
+        //q7
+//
+//
+//
+//        for (int j = 1; j <=4 ; j++) {
+//            System.out.println("week"+j);
+//           continue;
+//        }
+//
+//        for (int i = 1; i <=7; i++) {
+//            System.out.println(" day "+i);
+//        }
+//
+//
+//
+//    }}
